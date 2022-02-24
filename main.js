705382f7111df076a6ccbc13b1eb4be928a0d131
xLiwiax <boliwia333@gmail.com>
2022-02-24 23:51:40 +0100

    zadanie 18

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 let button = document.querySelector('button');
-let submit = document.getElementById("submit");
 
 function fun1 () {
-    let imie = document.getElementById("imie").textContent.value;
-    let nazwisko = document.getElementById("naz").value;
-    let telefon = document.getElementById("tel").value;
-    let napImie = document.getElementById("name");
-}
-function fun2 () {
-    napImie.textContent = imie;
-    let napNaz = document.getElementById("surname");
-    napNaz.textContent = nazwisko; 
-    let napTel = document.getElementById("phone");
-    napTel.textContent = telefon;  
+    let imie = document.getElementById("imie").value
+    let nazwisko = document.getElementById("naz").value
+    let telefon = document.getElementById("tel").value
+  
+    let napImie = document.getElementById("name")
+    napImie.textContent = imie
+    let napNaz = document.getElementById("surname")
+    napNaz.textContent = nazwisko
+    let napTel = document.getElementById("phone")
+    napTel.textContent = telefon
 }
 
-
-submit.onclick = fun1;
-button.onclick = fun2;
+button.onclick = fun1;
